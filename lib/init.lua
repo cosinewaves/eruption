0c2f45f97d738f2f417541cddf578319f0faa85e
cosinewaves <elkhaadnan@gmail.com>
2025-06-29 17:58:08 +0100

    add internal

--- lib/init.lua
+++ lib/init.lua
@@ -8,22 +8,60 @@ export type article = { [any]: any }
 export type argue<T> = (T) -> boolean
 export type eruption = {
     declareChildren: (container: Instance) -> (),
-    declareDescendants: (container: Instance) -> ()
+    declareDescendants: (container: Instance, argue: argue<Instance>?) -> (),
+    erupt: (self: eruption) -> (),
+}
+type internal = {
+    declared: { article },
+    erupted: boolean,
+    safeRequire: (module: ModuleScript) -> (),
 }
 
 -- Metatable
 local eruption = {} :: eruption
 setmetatable(eruption, {__index = eruption})
 
--- Internal
-local declared: {article} = {}
+-- Internal Metatable
+local internal = {
+     declared = {},
+     erupted = false
+} :: internal
+setmetatable(internal, {__index = internal})
 
 -- Functions
+
+local function saferequireAsync(module: ModuleScript): ()
+    return promise.new(function(resolve, reject)
+        local success, result = pcall(require, module)
+        if success then
+            resolve(result)
+        else
+            reject(result)
+        end
+    end)
+end
+
+
 function eruption.declareChildren(container: Instance): ()
     
 end
 
-function eruption.declareDescendants(container: Instance): ()
+function eruption.declareDescendants(container: Instance, argue: argue<Instance>?): ()
+    for _, descendant in container:GetDescendants() do
+
+        if argue and not argue(descendant) then 
+            continue 
+        end
+
+        if not descendant:IsA("ModuleScript") then
+            continue
+        end
+
+	end
+    return
+end
+
+function eruption:erupt(): ()
     
 end
 
